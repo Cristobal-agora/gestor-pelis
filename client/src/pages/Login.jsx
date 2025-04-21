27fb028231f26a0c0db3724bdd0ec7c9fbac37e4
Cristóbal <cristo.mates@gmail.com>
2025-04-21 14:57:55 +0200

    fix: cambiar URLs a VITE_API_URL

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -26,7 +26,7 @@ const registrado = location.state?.registrado || false;
     e.preventDefault();
   
     try {
-      const res = await axios.post('http://localhost:5000/api/login', formData);
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/login`, formData);
       setMensaje(res.data.mensaje);
       setToken(res.data.token);
   
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-
 const Register = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
     nombre: '',
@@ -20,28 +19,37 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     try {
-      const res = await axios.post('http://localhost:5000/api/register', formData);
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/register`, formData);
       setMensaje(res.data.mensaje);
-  
+
       // Redirigir al login con mensaje
       navigate('/login', { state: { registrado: true } });
     } catch (error) {
       console.error(error);
-      setMensaje(error.response?.data?.mensaje || 'Error en el registro');
+
+      if (error.response?.data?.errores) {
+        const errores = error.response.data.errores.map(err => `• ${err.msg}`).join('\n');
+        setMensaje(errores);
+      } else {
+        setMensaje(error.response?.data?.mensaje || 'Error en el registro');
+      }
     }
   };
-  
-  
 
   return (
     <div className="d-flex align-items-center justify-content-center" style={{ minHeight: '80vh' }}>
-      <div className="p-4 rounded shadow" style={{ maxWidth: '500px', width: '100%', backgroundColor: '#2a2a2a' }}
-      >
+      <div className="p-4 rounded shadow" style={{ maxWidth: '500px', width: '100%', backgroundColor: '#2a2a2a' }}>
         <h2 className="mb-4 text-center text-light">Registro</h2>
 
-        {mensaje && <div className="alert alert-info">{mensaje}</div>}
+        {mensaje && (
+          <div className="alert alert-info text-start">
+            {mensaje.split('\n').map((linea, index) => (
+              <div key={index}>{linea}</div>
+            ))}
+          </div>
+        )}
 
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
@@ -85,7 +93,6 @@ const Register = () => {
       </div>
     </div>
   );
-
 };
 
 export default Register;
