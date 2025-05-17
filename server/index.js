036e9126fb87b69cb0565997f23d5fb73a7d501f
CristobalVN <cristobal.munoz@viewnext.com>
2025-05-17 19:23:14 +0200

    .env nuevos

--- server/index.js
+++ server/index.js
@@ -38,5 +38,5 @@ app.get("/", (req, res) => {
 
 // Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });
